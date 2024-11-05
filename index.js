ac7d5f5dbb2f931a7ac34c2897c9562b3806635d
mwatad <53497154+mwatad@users.noreply.github.com>
2024-11-05 09:24:39 +0200

    Done

--- index.js
+++ index.js
@@ -3,3 +3,17 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const btn = document.getElementById("convert-btn")
+const input = document.getElementById("input-el")
+const length = document.getElementById("length")
+const volume = document.getElementById("volume")
+const mass = document.getElementById("mass")
+
+btn.addEventListener("click", function() {
+    number = Number(input.value)
+    console.log(number)
+    length.textContent = `${number} meters = ${(number * 3.281).toFixed(3)} feet | ${number} feet = ${(number / 3.281).toFixed(3)} meters`
+    volume.textContent = `${number} liters = ${(number * 0.264).toFixed(3)} gallons | ${number} gallons = ${(number / 0.264).toFixed(3)} liters`
+    mass.textContent = `${number} kilos = ${(number * 2.204).toFixed(3)} pounds | ${number} pounds = ${(number / 2.204).toFixed(3)} kilos`
+})
